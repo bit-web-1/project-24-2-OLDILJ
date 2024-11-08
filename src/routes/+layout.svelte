8168e215fd9a29cd90db5d4a21225d891529dc76
OLDILJ <OLDILJ1@op.ac.nz>
2024-11-08 14:02:07 +1300

    Finished With random Issues

--- src/routes/+layout.svelte
+++
@@ -1,7 +0,0 @@
-<script>
-    /** @type {import('./$types').LayoutData} */
-    export let data;
-    import "../lib/styles.css"
-</script>
-
-<slot />
--- src/routes/+page.svelte
+++
@@ -1,7 +0,0 @@
-<script>
-    import Test from "../lib/Test.svelte";
-    import Btn from "../lib/ButtonSelector.svelte";
-</script>
-<h1 class="title">Welcome</h1>
-<Btn></Btn>
-<Test></Test>
