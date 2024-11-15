ccacba70d43e83304a2e89d7a6fd949c26344d3a
OLDILJ <OLDILJ1@op.ac.nz>
2024-11-15 16:56:18 +1300

    added text to about page

---
+++ Assessment/src/lib/Footer.svelte
@@ -0,0 +1,19 @@
+<script>
+
+</script>
+
+<p>Logan j. Olding</p>
+
+
+
+
+<style>
+p
+{
+    background-color: sandybrown;
+    padding: 2em;
+    font-weight: bolder;
+    font-size: larger;
+    border: 0.25em dotted;
+}
+</style>
--- Assessment/src/routes/About/+page.svelte
+++ Assessment/src/routes/About/+page.svelte
@@ -4,5 +4,8 @@
     import NavBar from "$lib/NavBar.svelte";
 </script>
 <NavBar/>
-<h3>aaaa</h3>
-<p>Test123</p>
+<h3>What is this?</h3>
+<p>The purpose of this website was to create a little battle system using different api's for the main characters stats with a selection at the beginning to change how the characters are improved via api details</p>
+<h3>Why is it so barebones</h3>
+<p>Sveltekit sucks. Transfering data between different pages is a massive pain in the ass and the error messages just send you on wild goose chases leaving you with little to no way to figure out the mistake.</p>
+<p>on top of that the tutorials suck as they explain a concept halfway then stop and swap to another variation of the same thing without fully explaining every detail about it. They introduced databases just expecting the beginning user to understand how it works from first glance</p>
