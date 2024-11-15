d0f0a9c62ca1e42f31635c2327f21e459856655a
OLDILJ <OLDILJ1@op.ac.nz>
2024-11-15 16:19:21 +1300

    componentized the nav bar

---
+++ Assessment/src/lib/NavBar.svelte
@@ -0,0 +1,30 @@
+<ul>
+    <li>
+        <a href="/Home">Home</a>
+    </li>
+</ul>
+
+<style>
+    ul
+{
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+    width: 100px;
+
+    background-color:rgb(224, 125, 89);
+}
+li a
+{
+    color: black;
+    text-align: center;
+    padding: 4, 8;
+    display: block;
+    text-decoration: none;
+}
+li a:hover
+{
+    background-color: rgb(190, 104, 88);
+    color: wheat;
+}
+</style>
--- Assessment/src/routes/About/+page.svelte
+++ Assessment/src/routes/About/+page.svelte
@@ -1,4 +1,8 @@
 <script>
     /** @type {{ data: import('./$types').PageData }} */
     let { data } = $props();
+    import NavBar from "$lib/NavBar.svelte";
 </script>
+<NavBar/>
+<h3>aaaa</h3>
+<p>Test123</p>
