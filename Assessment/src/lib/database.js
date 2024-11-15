97ad5b834d66186068dd30a8b12190bdab26eced
OLDILJ <OLDILJ1@op.ac.nz>
2024-11-15 16:08:27 +1300

    New page new Database problems

--- Assessment/src/lib/database.js
+++ Assessment/src/lib/database.js
@@ -8,25 +8,36 @@ export function getCurRun(userid) {
 		}]);
 	}
 
-	return db.get(userid);
+	$: return db.get(userid);
+}
+export function getId()
+{
+    if (!db.get(db.userid))
+        {
+            db.set(crypto.randomUUID(), [{
+                id: crypto.randomUUID(),
+                description: 'New Player',
+            }]);
+        }
+        return(db.userid);
 }
 export function setChar(selectedChar)
 {
-    if (!db.get(db.userid))
+    $: if (!db.get(db.userid))
         {
             db.set(db.userid, [{
                 id: crypto.randomUUID(),
                 description: 'New Player',
             }]);
         }
-    db.set(db.userid, [{
+        db.set(db.userid, [{
         CurRunChar: selectedChar
     }])
-    return db.get(db.userid,db.CurRunChar);
-    console.log(db.userid, db.CurRunChar)
+    $: return db.get(db.userid,db.CurRunChar);
+    $: console.log(db.userid, db.CurRunChar)
 }
 export function getChar()
 {
-    console.log(db.CurRunChar, "test")
-    return db.get(db.CurRunChar)
+    $: console.log(db.CurRunChar, "test")
+    $: return db.get(db.CurRunChar)
 }
--- Assessment/src/routes/Main/+page.svelte
+++ Assessment/src/routes/Main/+page.svelte
@@ -3,8 +3,9 @@
     let { data } = $props();
     import CharSelect from '../../lib/CharSelect.svelte';
     import * as db from '$lib/database.js';
-    var CurRunChar = db.getChar();
+    var CurRunChar = db.CurRunChar;
+    var userid = db.userid;
 </script>
 
 <h1>Hello {data.visited ? 'friend' : 'stranger'}!</h1>
-<p>You are {CurRunChar}</p>
+<p>You are {CurRunChar} And You are User: {userid} </p>
