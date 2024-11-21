4613b834cad92cff2b710a5f2481eea39ba86fa1
OLDILJ <OLDILJ1@op.ac.nz>
2024-11-22 11:51:49 +1300

    Set basic page text styling up and started on weather bar

--- Assessment/src/routes/About/+page.svelte
+++ Assessment/src/routes/About/+page.svelte
@@ -2,6 +2,18 @@
 <script>
     import Head from "$lib/Header.svelte";
     import Bar from "$lib/NavBar.svelte";
+    import "$lib/TextStyling.css";
 </script>
 <Head/>
 <Bar/>
+<h1>About</h1>
+<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Rem, non!</p>
+<p>Ullam, sed beatae ad consectetur enim laboriosam vel iusto natus.</p>
+<p>Dolore quisquam quis atque neque aut fugiat non illo iste?</p>
+<p>Id distinctio consequuntur quaerat eum maxime quam repellat illum tempora.</p>
+<p>Quo neque deleniti, delectus nisi provident recusandae enim explicabo quos.</p>
+<p>Laborum explicabo rem odio animi sequi incidunt atque ducimus harum.</p>
+<p>Dignissimos reiciendis, maxime sint unde dicta quos rerum quas adipisci?</p>
+<p>Ratione eos aspernatur nihil alias pariatur autem. Obcaecati, in harum!</p>
+<p>Tenetur exercitationem cupiditate odit itaque nam excepturi reprehenderit praesentium cumque.</p>
+<p>Maiores doloribus alias enim, optio ab magni repellendus minima illo.</p>
