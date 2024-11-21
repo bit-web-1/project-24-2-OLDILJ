6e5c4cc29d7dded3cde67c48672a54dad22e23d4
OLDILJ <OLDILJ1@op.ac.nz>
2024-11-22 10:34:06 +1300

    added basic header + styling

---
+++ Assessment/src/lib/Header.svelte
@@ -0,0 +1,27 @@
+<script>
+    import Mascot from "$lib/Images/TransparentMascotGuy.png";
+</script>
+<div class="FloatContainer">
+    <h1>Normal News</h1>
+    <img src={Mascot} alt=""/>
+</div>
+<style>
+    .FloatContainer
+    {
+        width: 80%;
+        padding-left: 5%;
+        padding-bottom: 10%;
+        justify-items: center;
+    }
+    img
+    {
+        
+        vertical-align: middle;
+    }
+    h1
+    {
+        text-align: center;
+        display: inline-block;
+        vertical-align:middle;
+    }
+</style>
--- Assessment/src/routes/+page.svelte
+++ Assessment/src/routes/+page.svelte
@@ -1,2 +1,6 @@
+<script>
+    import Head from "$lib/Header.svelte";
+</script>
+<Head/>
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
